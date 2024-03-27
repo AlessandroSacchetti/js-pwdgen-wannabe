50bede3efdde5337e5d1bf893ce5f370523e26b2
AlessandroSacchettiB <a.sac.ben@gmail.com>
2024-03-27 10:09:03 +0100

    Push: whole exercise

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let userName = prompt('Inserisci il tuo Nome'); // Richiesta del nome
+console.log(userName)
+
+let userSurname = prompt('Inserisci il tuo Cognome'); // Richiesta del cognome
+console.log(userSurname)
+
+let userColor = prompt('Il tuo colore preferito'); // Richiesta del colore preferito
+console.log(userColor)
+
+let randomizer = Math.floor((Math.random() * 100) + 1); // Numero intero casuale tra 1 e 100
+console.log(randomizer)
+console.log(userName + userSurname + userColor + randomizer) // sequenza di stringhe ovvero PWD-WANNABE
